a03b71119e7186f17400a29df1531b61ebeb7aec
Fred Pallesen <mail@xparagon.no>
2023-05-13 13:31:38 +0200

    more

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,4 +1,4 @@
-import { Title, Text, Anchor, Container, Space, Grid, Divider, Center, Button } from '@mantine/core';
+import { Title, Text, Anchor, Container, Space, Grid, Divider, Center, Button, Flex } from '@mantine/core';
 import useStyles from './Welcome.styles';
 import JoinDiscord from '../JoinDiscord/JoinDiscord';
 import { MainMenu } from '../MainMenu/MainMenu';
@@ -6,23 +6,23 @@ import { MainFooter } from '../MainFooter/MainFooter';
 import MainTopMenu from '../MainTopMenu/MainTopMenu';
 import MainLayout from '../../pages/layout';
 import { useState } from 'react';
+import { IconEye } from '@tabler/icons';
 
 export function Welcome() {
   // const { classes } = useStyles();
 
 
-  const [ufoImg, setUfoImg] = useState('/img/ph-1.jpg');
+  const [imgUfo, imgUfoSet] = useState('/img/ph-1.jpg');
 
-  function doNextUfoImg(i: string) {
-    setUfoImg(i);
-  }
+  const [imgEquipment, imgEquipmentSet] = useState('/img/ph-2.jpg');
+
 
 
   return (
     <MainLayout>
 
+      <Space h="sm" />
       <Divider my="sm" />
-
       <Space h="sm" />
       <Text ta="center" c="blue" size={48}>
         Hessdalen is a UFO hotspot.
@@ -35,50 +35,97 @@ export function Welcome() {
 
       <Grid>
         <Grid.Col sm={6}>
-          <Center mx="auto">
-            <img src={ufoImg} width="100%" />
+          <Flex
+            mih='100%'
+            justify="center"
+            align="center"
+            direction="column"
+          >
+            <img src={imgUfo} width="100%" />
+            <Grid pt={4}>
+              <Grid.Col span={3}><Button variant="outline" compact>
+                1
+              </Button></Grid.Col>
+              <Grid.Col span={3}><Button variant="outline" compact>
+                2
+              </Button></Grid.Col>
+              <Grid.Col span={3}><Button variant="outline" compact>
+                3
+              </Button></Grid.Col>
+              <Grid.Col span={3}><Button variant="outline" compact>
+                4
+              </Button></Grid.Col>
+            </Grid>
 
-          </Center>
+          </Flex>
         </Grid.Col>
         <Grid.Col sm={6}>
           <Text tt="uppercase">The Phenomena</Text>
-          <div onMouseEnter={() => setUfoImg('/img/ph-4.jpg')} style={{ cursor: 'pointer' }}  >
+          <div onMouseEnter={() => imgUfoSet('/img/ph-4.jpg')} style={{ cursor: 'pointer' }}  >
 
             <Title order={2}>
               Type 1
             </Title>
 
-            <Text>
-              White, or blue-white flashing light.
-            </Text>
+            <Flex
+              mih={32}
+              gap="xs"
+              justify="flex-start"
+              align="center"
+              direction="row"
+            >
+              <IconEye color='lightblue' size={24} />
+              <Text>
+                White, or blue-white flashing light.
+              </Text>
+            </Flex>
             <Text color="dimmed" size="xs">
               They are usually high up in the air, close to the to of the mountains or even higher.
               Short lifespan, seconds to minutes.
             </Text>
           </div>
           <Space h="md" />
-          <div onMouseEnter={() => setUfoImg('/img/ph-1.jpg')} style={{ cursor: 'pointer' }}>
+          <div onMouseEnter={() => imgUfoSet('/img/ph-1.jpg')} style={{ cursor: 'pointer' }}>
 
             <Title order={2}>
               Type 2
             </Title>
-            <Text>
-              A yellow light, with a red light on the top.
-            </Text>
+            <Flex
+              mih={32}
+              gap="xs"
+              justify="flex-start"
+              align="center"
+              direction="row"
+            >
+              <IconEye color='orange' size={24} />
+              <Text>
+                A yellow light, with a red light on the top.
+              </Text>
+            </Flex>
+
             <Text color="dimmed" size="xs">
               The red light can be flashing.
               The yellow light varies in intensity.
             </Text>
           </div>
           <Space h="md" />
-          <div onMouseEnter={() => setUfoImg('/img/ph-5.jpg')} style={{ cursor: 'pointer' }} >
+          <div onMouseEnter={() => imgUfoSet('/img/ph-5.jpg')} style={{ cursor: 'pointer' }} >
 
             <Title order={2}>
               Type 3
             </Title>
-            <Text>
-              The shape is often round as a ball.
-            </Text>
+            <Flex
+              mih={32}
+              gap="xs"
+              justify="flex-start"
+              align="center"
+              direction="row"
+            >
+              <IconEye color='red' size={24} />
+              <Text>
+                The shape is often round as a ball.
+              </Text>
+            </Flex>
             <Text color="dimmed" size="xs">
               <i>This is the most common description of the Hessdalen Phenomena.</i><br />
               A yellow or white light.
@@ -87,14 +134,23 @@ export function Welcome() {
             </Text>
           </div>
           <Space h="md" />
-          <div onMouseEnter={() => setUfoImg('/img/ph-7.jpg')} style={{ cursor: 'pointer' }} >
+          <div onMouseEnter={() => imgUfoSet('/img/ph-7.jpg')} style={{ cursor: 'pointer' }} >
 
             <Title order={2}>
               Type 4
             </Title>
-            <Text>
-              A black "object".
-            </Text>
+            <Flex
+              mih={32}
+              gap="xs"
+              justify="flex-start"
+              align="center"
+              direction="row"
+            >
+              <IconEye size={24} />
+              <Text>
+                A black "object".
+              </Text>
+            </Flex>
             <Text color="dimmed" size="xs">
               This image closely resembles the phenomena described in many witness reports.
 
@@ -106,79 +162,137 @@ export function Welcome() {
         </Grid.Col>
       </Grid>
 
-      <Space h="sm" />
-      <Divider my="sm" />
+
 
       <Space h="sm" />
+      <Divider my="sm" />
+      <Space h="sm" />
       <Text ta="center" c="orange" size={48}>
-        24/7 Automatic Surveillance System
+        24/7 Automatic UFO Surveillance
       </Text>
       <Text ta="center" size={24}>
-        When the light phenomena appears, or a UFO, the system will <b>automatically</b> start recording.
-        And you can get a notification on your phone *
+        When the light phenomena appears, a UFO or a mysterious Energy Object,
+        the system will <b>automatically</b> start recording -
+        and you can get a notification on your mobilephone<sup>*</sup>
       </Text>
       <Space h="xl" />
 
       <Grid>
         <Grid.Col sm={6}>
-          <Center mx="auto">
-            <img src="https://dummyimage.com/400x300/efefef/ffffff&text=blue+box+camera" />
-          </Center>
+          <Flex
+            mih='100%'
+            justify="center"
+            align="center"
+            direction="row"
+          >
+            <img src={imgEquipment} width="100%" />
+
+          </Flex>
         </Grid.Col>
         <Grid.Col sm={6}>
-          <Title order={2}>
-            The Blue Box
-          </Title>
-          <Text color="dimmed" size="xs">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, natus error sit voluptatem
-          </Text>
-          <Space h="md" />
-          <Title order={2}>
-            Live Streaming
-          </Title>
-          <Text color="dimmed" size="xs">
-            Totam rem aperiam, eaque
-            ipsa quae ab illo inventore veritatis et quasi architectobeatae
-            vitae dicta sunt explicabo.
-            Totam rem aperiam, eaque
-            ipsa quae ab illo inventore veritatis et quasi architectobeatae
-            vitae dicta sunt explicabo.
-          </Text>
-          <Space h="md" />
-        </Grid.Col>
+          <Text tt="uppercase">The Phenomena</Text>
+          <div onMouseEnter={() => imgEquipmentSet('/img/ph-2.jpg')} style={{ cursor: 'pointer' }}  >
 
+            <Title order={2}>
+              The Blue Box
+            </Title>
+
+            <Flex
+              mih={32}
+              gap="xs"
+              justify="flex-start"
+              align="center"
+              direction="row"
+            >
+              <IconEye color='lightblue' size={24} />
+              <Text>
+                White, or blue-white flashing light.
+              </Text>
+            </Flex>
+            <Text color="dimmed" size="xs">
+              They are usually high up in the air, close to the to of the mountains or even higher.
+              Short lifespan, seconds to minutes.
+            </Text>
+          </div>
+          <Space h="md" />
+          <div onMouseEnter={() => imgEquipmentSet('/img/ph-9.jpg')} style={{ cursor: 'pointer' }}>
+
+            <Title order={2}>
+              Live Streaming
+            </Title>
+            <Flex
+              mih={32}
+              gap="xs"
+              justify="flex-start"
+              align="center"
+              direction="row"
+            >
+              <IconEye color='orange' size={24} />
+              <Text>
+                A yellow light, with a red light on the top.
+              </Text>
+            </Flex>
+
+            <Text color="dimmed" size="xs">
+              The red light can be flashing.
+              The yellow light varies in intensity.
+            </Text>
+          </div>
+          <Space h="md" />
+          <div onMouseEnter={() => imgEquipmentSet('/img/ph-10.jpg')} style={{ cursor: 'pointer' }} >
+
+            <Title order={2}>
+              Scientific Goal
+            </Title>
+            <Flex
+              mih={32}
+              gap="xs"
+              justify="flex-start"
+              align="center"
+              direction="row"
+            >
+              <IconEye size={24} />
+              <Text>
+                Doing exactly what we do in Astronomy.
+              </Text>
+            </Flex>
+            <Text color="dimmed" size="xs">
+              Understanding the Physical Mechanism of what is observed by studying the Variability of the Phenomena and the correlation of different
+              physical parameters, through simultaneous multi wavelength observations.
+            </Text>
+          </div>
+          <Space h="md" />
+          <div onMouseEnter={() => imgEquipmentSet('/img/ph-8.jpg')} style={{ cursor: 'pointer' }} >
+
+            <Title order={2}>
+              Radar + Magnetometer
+            </Title>
+            <Flex
+              mih={32}
+              gap="xs"
+              justify="flex-start"
+              align="center"
+              direction="row"
+            >
+              <IconEye size={24} />
+              <Text>
+                A black "object".
+              </Text>
+            </Flex>
+            <Text color="dimmed" size="xs">
+              This image closely resembles the phenomena described in many witness reports.
+
+              (This is from a film recorded in Hungary in 1990.)
+            </Text>
+          </div>
+          <Space h="md" />
+
+        </Grid.Col>
       </Grid>
-      <Space h="xl" />
 
-      <Grid mt="sm">
-        <Grid.Col sm={6}>
-          <Center maw={400} h={250} mx="auto">
-            <img src="https://dummyimage.com/400x300/efefef/ffffff&text=alarm+radar" />
 
-          </Center>
-        </Grid.Col>
-        <Grid.Col sm={6}>
-          <Title order={2}>
-            Automatic Alarm System
-          </Title>
-          <Text color="dimmed" size="xs">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium,
-          </Text>
-          <Space h="md" />
-          <Title order={2}>
-            Radar + Magnetometer
-          </Title>
-          <Text color="dimmed" size="xs">
-            Totam rem aperiam, eaque
-            ipsa quae ab illo inventore veritatis et quasi architectobeatae
-            vitae dicta sunt explicabo.
-          </Text>
-          <Space h="md" />
-        </Grid.Col>
 
-      </Grid>
+
 
       <Space h="sm" />
       <Divider my="sm" />
